d6a0be102bd7b63f0fa20716c41d4d28d6953d49
Ravi Peters <ravi@ravis-imac.ws.tudelft.net>
2013-09-24 16:46:17 +0200

    Added GPL v3 licensing

--- doublebuffer/DoubleBuffer.h
+++ doublebuffer/DoubleBuffer.h
@@ -1,10 +1,21 @@
-//
-//  DoubleBuffer.h
-//  doublebuffer
-//
-//  Created by Ravi on 15/5/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
+// Copyright (c) 2012, 2013
+// Ravi Peters -- r.y.peters@tudelft.nl
+// All rights reserved
+// 
+// This file is part of Surfonoi.
+// 
+// Surfonoi is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Surfonoi is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Surfonoi.  If not, see <http://www.gnu.org/licenses/>.
 
 // (see http://trac.osgeo.org/geos/ticket/548 ) Patch is in GEOS trunk now and planned to be in GEOS 3.4
 //#define WITHGEOSBIL
