f686f8cec7e02a7f487337c0ceee817c1ad122c4
Willem van Opstal <willemvanopstal@home.nl>
2018-04-12 14:19:49 +0200

    add simple visualisation

--- establisher.py
+++ establisher.py
@@ -5,30 +5,28 @@ import time
 from pysurfonoi import *
 
 def establishPoints(source):
-	with open(source) as srci:
-		identifier = 0
-		for line in srci.readlines()[1:]:
-			itemList = []
-			for item in line.split(';'):
-				itemList.append(float(item))
-			Measurement(itemList, identifier)
-			identifier += 1
-	return
+    with open(source) as srci:
+        identifier = 0
+        for line in srci.readlines()[1:]:
+            itemList = []
+            for item in line.split(','):
+                itemList.append(float(item))
+            Measurement(itemList, identifier)
+            identifier += 1
+    return
 
 
 startTime = time.time()
 
-establishPoints('hdwest.csv')
+establishPoints('small_portion.csv')
 #Measurement.asCsv('set_points.csv')
-print '--------------------------------\ntotal objects:\t\t\t', Measurement.totalObjects()
+print '--------------------------------\ntotal objects:\t\t', Measurement.totalObjects()
 
 midTime = time.time()
 print 'initiated points in:\t', midTime - startTime
 
-Measurement.fullDelaunay()
-Measurement.fullVoronoi()
+Measurement.establishNetwork()
 Measurement.establishNeighbors()
-
 midmidTime = time.time()
 print 'network initited in:\t', midmidTime - midTime
 
@@ -37,10 +35,13 @@ Measurement.updateQueue()
 
 endTime = time.time()
 print 'iterated and updated:\t', endTime - midmidTime
-print 'total elapsed:\t\t\t', endTime - startTime
+print 'total elapsed:\t\t', endTime - startTime
 print '--------------------------------\n'
 
 Measurement.asCsv('one_iteration_later.csv')
+print 'csv exported'
 
-
+print 'starting visualization'
 #Measurement.printAllInstances()
+Measurement.visTin()
+
--- pysurfonoi.py
+++ pysurfonoi.py
@@ -3,13 +3,39 @@
 
 import numpy as np
 from scipy.spatial import Delaunay, Voronoi, voronoi_plot_2d
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 from math import sqrt
+import time
+
+def set_aspect_equal_3d(ax):
+    """Fix equal aspect bug for 3D plots.
+    https://stackoverflow.com/a/35126679/5011857"""
+
+    xlim = ax.get_xlim3d()
+    ylim = ax.get_ylim3d()
+    zlim = ax.get_zlim3d()
+
+    from numpy import mean
+    xmean = mean(xlim)
+    ymean = mean(ylim)
+    zmean = mean(zlim)
+
+    plot_radius = max([abs(lim - mean_)
+                       for lims, mean_ in ((xlim, xmean),
+                                           (ylim, ymean),
+                                           (zlim, zmean))
+                       for lim in lims])
+
+    ax.set_xlim3d([xmean - plot_radius, xmean + plot_radius])
+    ax.set_ylim3d([ymean - plot_radius, ymean + plot_radius])
+    ax.set_zlim3d([zmean - plot_radius, zmean + plot_radius])
 
 def distance(xone, yone, xtwo, ytwo):
-	xdif = pow((xtwo - xone),2)
-	ydif = pow((ytwo - yone),2)
-	return sqrt(xdif + ydif)
+    xdif = pow((xtwo - xone),2)
+    ydif = pow((ytwo - yone),2)
+    return sqrt(xdif + ydif)
 
 def ccw(Ax, Bx, Cx, Ay, By, Cy):
     return (Cy-Ay) * (Bx-Ax) > (By-Ay) * (Cx-Ax)
@@ -22,8 +48,9 @@ class Measurement(object):
     _instances = []
     _easyIndex = {}
 
+    depth_queue = None
+
     def __init__(self, initList, identification):
-        self.depth_queue = None
         self.updateNr = 0
         self.neighbors = set()
         self.voronoiRing = []
@@ -50,8 +77,77 @@ class Measurement(object):
                 print item.identification
                 print item.depth_measurement
                 print item.depth_current
+                print item.depth_queue
                 print item.updateNr
-                #print item.depth_queue
+                # print item.depth_queue
+
+    @classmethod
+    def findMinMax(cls):
+        xmin = 9999999999.9
+        xmax = 0.0
+        ymin = 9999999999.9
+        ymax = 0.0
+
+        dmin = -2.5
+        dmax = 0
+
+        for item in cls._instances:
+            if item.loc_x < xmin:
+                xmin = item.loc_x
+            if item.loc_x > xmax:
+                xmax = item.loc_x
+            if item.loc_y < ymin:
+                ymin = item.loc_y
+            if item.loc_y > ymax:
+                ymax = item.loc_y
+            if item.depth_current > dmax:
+                dmax = item.depth_current
+            if item.depth_current < dmin:
+                dmin = item.depth_current
+
+        cls.xmin = xmin
+        cls.xmax = xmax
+        cls.ymin = ymin
+        cls.ymax = ymax
+        cls.dmin = dmin
+        cls.dmax = dmax
+
+    @classmethod
+    def visTin(cls):
+        #https://stackoverflow.com/questions/43393162/how-to-plot-3d-triangles-in-matplotlib-with-triangles-verticess-coordinates-9
+        visTime = time.time()
+        cls.findMinMax()
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        fList = []
+        for triangle in cls.DT.points[cls.DT.simplices]:
+            vList = []
+            for vertex in triangle:
+                vList.append(vertex[0])
+                vList.append(vertex[1])
+                vList.append(cls._easyIndex[str(vertex)].depth_current)
+            vArray = np.array(vList)
+            fList.append(vArray)
+        fArray = np.array(fList)
+        print fArray[0][-1], fArray[0][-4], fArray[0][-7]
+
+        cmap = cm.get_cmap('ocean')
+        poly3d = [[ fArray[i, j*3:j*3+3] for j in range(3)  ] for i in range(fArray.shape[0])]
+        fc = [cmap(((fArray[i][-1]+fArray[i][-4]+fArray[i][-7])/3)/(cls.dmax-cls.dmin)) for i in range(fArray.shape[0])]
+
+        ax.add_collection3d(Poly3DCollection(poly3d, facecolors=fc, linewidths=0))
+        ax.set_xlim(cls.xmin, cls.xmax)
+        ax.set_ylim(cls.ymin, cls.ymax)
+        ax.set_zlim(cls.dmin, cls.dmax)
+        ax.invert_zaxis()
+
+        print 'visualizationTime:\t', time.time()-visTime
+        #plt.gca().set_aspect('equal', adjustable='box')
+        ax.set_aspect(1)
+        #set_aspect_equal_3d(ax)
+        plt.show()
 
     @classmethod
     def asCsv(cls, filepath):
@@ -74,12 +170,21 @@ class Measurement(object):
 
     @classmethod
     def fullDelaunay(cls):
+        #gone
         array = cls.npArray()
         cls.DT = Delaunay(array)
-        return Delaunay(array)
+        return Delaunay(array)    \
+
+    @classmethod
+    def establishNetwork(cls):
+        array = cls.npArray()
+        cls.DT = Delaunay(array)
+        cls.VD = Voronoi(array)
+        return
 
     @classmethod
     def fullVoronoi(cls):
+        #gone
         array = cls.npArray()
         cls.VD = Voronoi(array)
         return Voronoi(array)
@@ -110,26 +215,26 @@ class Measurement(object):
         for k, point in enumerate(cls.VD.points):
 
             ring = cls.VD.point_region[k]
-            if -1 in cls.VD.regions[ring]:
-                continue
-            else:
+            if -1 not in cls.VD.regions[ring]:
                 vList = []
                 for i in range(len(cls.VD.regions[ring])):
                     if not cls.VD.regions[ring][i] == -1:
                         vList.append(cls.VD.vertices[cls.VD.regions[ring][i]])
                     else:
-                        print ''
+                        continue
                 if not cls.VD.regions[ring][0] == -1:
                     vList.append(cls.VD.vertices[cls.VD.regions[ring][0]])
                 else:
-                    print ''
+                    continue
 
                 index[str(point)].voronoiRing = vList
-
+            else:
+                continue
         return
 
     @classmethod
     def iterateAll(cls):
+        queued = 0
         for item in cls._instances:
             sumOfWeights = 0.0
             upper = 0.0
@@ -150,6 +255,7 @@ class Measurement(object):
                         checker = intersect(item.loc_x, neighbor.loc_x, item.voronoiRing[p][0], item.voronoiRing[ p +1][0], item.loc_y, neighbor.loc_y, item.voronoiRing[p][1], item.voronoiRing[ p +1][1])
                         if checker:
                             vdDistance = distance(item.voronoiRing[p][0], item.voronoiRing[p][1], item.voronoiRing[ p +1][0], item.voronoiRing[ p +1][1])
+                            break
                         # print 'VD distance:\t{0}'.format(vdDistance)
                     weightNbr = vdDistance /dtDistance
                     sumOfWeights += weightNbr
@@ -159,6 +265,9 @@ class Measurement(object):
                 # print 'interpolated depth:\t{0}'.format(interpolated_depth)
                 if interpolated_depth < item.depth_current:
                     item.depth_queue = interpolated_depth
+                    queued += 1
                 else:
-                    item.depth_queue = None
-                # print 'queue:\t\t:{0}'.format(item.depth_queue)
+                    continue
+        print 'queued points:\t\t', queued
+        # print 'queue:\t\t:{0}'.format(item.depth_queue)
+        #return
